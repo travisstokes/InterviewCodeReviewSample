b5545c0f4c5df3905efd0b3db1635c4f814fd0c2
Travis Stokes <travis@sysgineer.com>
2015-08-11 16:19:55 -0400

    Initial commit for sharing

---
+++ InterviewCodeReview/EmailNotification.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCodeReview
+{
+    public interface INotificationService
+    {
+        void SendNotification(string email, char[] subject, string body, Int64 notificationType);  
+    }
+
+    public class emailNotificationService : INotificationService
+    {
+        public static string Host;
+        public static int Port;
+
+        public emailNotificationService(string host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+
+        public void SendNotification(string email, char[] subject, string body, Int64 notificationType)
+        {
+            SmtpClient client = new SmtpClient(Host, Port);
+            var from = string.Empty;
+
+            switch (notificationType)
+            {
+                case 1:
+                    from = "admin";
+                    break;
+                case 2:
+                    from = "support";
+                    break;
+                case 3: 
+                    from = "billing";
+                    break;
+            }
+
+            from = from + "@testapp.com";
+
+            try
+            {
+                client.Send(from, email, new string(subject), body);
+            }
+            catch(Exception ex)
+            {
+                LogHelper.LogError(ex);
+                throw ex;
+            }
+        }
+    }
+}
---
+++ InterviewCodeReview/LogHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewCodeReview
+{
+    public static class LogHelper
+    {
+        public static void LogError(Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
